14ac665dcf435585e65e99dd3edff0c1e7889be1
Satyanarayana Vadapalli <satyanarayana.v@eagleview.com>
2022-08-29 09:10:10 +0530

    Update locals.tf

--- terraform/amp/locals.tf
+++ terraform/amp/locals.tf
@@ -3,7 +3,6 @@ locals {
   resource_name_prefix = local.test_build ? "tb-${var.build_number}-sbox-1x0" : module.config.environment_config_map.resource_name_prefix
   region               = module.config.environment_config_map.region
   account_id           = module.config.environment_config_map.account_id
-  document_db_secret   = module.config.environment_config_map.property_data_orchestration_secret
   default_provider_tags = {
     "evtech:environment"    = module.config.environment_config_map.environment
     "evtech:owner"          = module.config.environment_config_map.evtech_owner
