ab52eedeab5fff105ce9705c2ff94a160d8380b4
Satyanarayana Vadapalli <satyanarayana.v@eagleview.com>
2022-09-01 10:20:57 +0530

    Update roles.tf

--- terraform/amp/roles.tf
+++ terraform/amp/roles.tf
@@ -1,17 +1,16 @@
-resource "aws_iam_role" "account-management" {
-  assume_role_policy = <<POLICY
-${module.config.environment_config_map.trust_relashionships_external_service}
-  POLICY
-
-  managed_policy_arns = []
-
-  max_session_duration = "3600"
-  name                 = "${local.resource_name_prefix}-role-account-management"
-  path                 = "/"
-
-  tags = {
-    Name        = "${local.resource_name_prefix}-role-account-management"
-    Description = "AWS IAM role to allow services to access platform-data-orchestrator common resources like Lambda"
-  }
+resource "aws_iam_role" "main" {
+    count = length(module.config.role_config_map)
+    assume_role_policy = <<POLICY
+${module.config.role_config_map[count.index].assume_role_policy}
+    POLICY
+    
+    inline_policy {
+        name   = "${module.config.role_config_map[count.index].inline_policy_name}"
+        policy = <<POLICY
+${module.config.role_config_map[count.index].inline_policy}
+        POLICY
+    }
+    max_session_duration = "3600"
+    name                 = "${local.resource_name_prefix}-role-${module.config.role_config_map[count.index].amp_role_name}"
+    path                 = "/"
 }
-
