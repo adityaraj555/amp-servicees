5baa2be4b9883527ec6ce8bec43560c53f360d8a
Satyanarayana Vadapalli <satyanarayana.v@eagleview.com>
2022-09-14 10:37:56 +0530

    Update main.tf

--- terraform/amp/main.tf
+++ terraform/amp/main.tf
@@ -4,17 +4,14 @@ module "config" {
 }
 
 module "batch" {
-  source = "git::https://github.eagleview.com/infrastructure/terraform-cloudops-module-aws-batch.git//batch/?ref=0.0.1"
+  source = "git::https://github.eagleview.com/infrastructure/terraform-cloudops-module-aws-batch.git//batch/?ref=CL-93"
 
   for_each = module.config.batch_configmap
 
-  resource_name_prefix  = local.resource_name_prefix
-  vpc_id                = each.value.vpc_id
-  subnet_id             = each.value.subnet_id
-  batch_name            = each.key
-  container_properties  = each.value.container_properties
-  compute_environment_name = each.value.compute_environment_name
-  instance_type         = each.value.instance_type
+  resource_name_prefix     = local.resource_name_prefix
+  batch_name               = each.key
+  container_properties     = each.value.container_properties
+  compute_environments     = each.value.compute_environments
 }
 
 data "aws_caller_identity" "current" {}
